de1caf4f87f4adad884424cdf8ca1abfde419e5d
Nikole26 <nikolemonterocervantes2026@u.northwestern.edu>
2024-05-09 01:07:44 -0500

    comparison models recipe 2 done - attempt 2

--- 02_attempt/4_model_analysis.R
+++ 02_attempt/4_model_analysis.R
@@ -10,22 +10,20 @@ library(here)
 tidymodels_prefer()
 
 # load data
-load(here("01_attempt/results/tune_mars_1.rda"))
-load(here("01_attempt/results/nn_tune_1.rda"))
-load(here("01_attempt/results/tictoc_mars_1.rda"))
-load(here("01_attempt/results/tictoc_mlp_1.rda"))
-load(here("01_attempt/results/knn_tune_1.rda"))
-load(here("01_attempt/results/tictoc_knn_1.rda"))
-load(here("01_attempt/results/rf_tune_1.rda"))
-load(here("01_attempt/results/tictoc_rf_1.rda"))
+load(here("02_attempt/results/tune_mars_2.rda"))
+load(here("02_attempt/results/nn_tune_2.rda"))
+load(here("02_attempt/results/knn_tune_2.rda"))
+load(here("02_attempt/results/rf_tune_2.rda"))
+load(here("02_attempt/results/bt_tuned_2.rda"))
 
 # comparison table
 model_set <-
   as_workflow_set(
-    "mars_1" = tune_mars_1,
-    "nn_1" = nn_simple_tune,
-    "knn_1" = knn_tune_1,
-    "rf_1" = rf_tune_1
+    "mars_2" = tune_mars_2,
+    "nn_2" = nn_tune_2,
+    "knn_2" = knn_tune_2,
+    "rf_2" = rf_tune_2,
+    "bt_2" = bt_tuned_2
   )
 
 models_table <- model_set |> 
@@ -36,12 +34,8 @@ models_table <- model_set |>
   slice_max(mean, with_ties = FALSE) |>
   select(wflow_id, roc_auc = mean, std_err, n, model) |>
   ungroup() |>
-  mutate(recipe = c("recipe 1", "recipe 1", "recipe 1", "recipe 1")
-         #runtime = c(tictoc_mars_1$runtime,
-          #           tictoc_mlp_simple$runtime,
-           #          tictoc_knn_1$runtime)
-         ) |>
+  mutate(recipe = c("recipe 2", "recipe 2", "recipe 2", "recipe 2", "recipe 2")) |> 
   select(-wflow_id) |>
   arrange(desc(roc_auc))
 
-save(models_table, file = here("01_attempt/results/models_table.rda"))
+save(models_table, file = here("02_attempt/results/models_table.rda"))
