679a68a09d847c3a18fed170f63fcfbe24ad0b9d
Nikole26 <nikolemonterocervantes2026@u.northwestern.edu>
2024-05-14 12:36:38 -0500

    initial set up - attempt 4

--- 04_attempt/1_set_up.R
+++ 04_attempt/1_set_up.R
@@ -11,11 +11,11 @@ library(ggplot2)
 tidymodels_prefer()
 
 # read in training data -------
-load(here("02_attempt/data/training_data.rda"))
+load(here("04_attempt/data/training_data.rda"))
 
 # create resamples (10-fold cv with 4 repeats) ----------
-set.seed(2500)
+set.seed(1436)
 air_bnb_folds <- training_data |>
   vfold_cv(v = 10, repeats = 5, host_is_superhost)
 
-save(air_bnb_folds, file = here("03_attempt/data/air_bnb_folds.rda"))
+save(air_bnb_folds, file = here("04_attempt/data/air_bnb_folds.rda"))
