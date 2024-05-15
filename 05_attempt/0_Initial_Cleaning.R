b17453675358567c60abf75a99f9d5a962744aed
Nikole26 <nikolemonterocervantes2026@u.northwestern.edu>
2024-05-15 09:41:33 -0500

    recipe 1 set up

--- 05_attempt/0_Initial_Cleaning.R
+++ 05_attempt/0_Initial_Cleaning.R
@@ -14,38 +14,6 @@ air_bnb_dictionary <- readxl::read_excel(here("data/data_dictionary.xlsx"))
 air_bnb_test_data <- read_csv(here("data/test_classification.csv"),
                           col_types = cols(id = col_character()))
 
-# Checking missingness-------
-skimr::skim_without_charts(air_bnb_data)
-skimr::skim_without_charts(air_bnb_test_data)
-
-## Most missingness in host_variables and review_scores_variables
-
-# Dealing with missingness-----
-summary(air_bnb_data$beds)
-
-air_bnb_data |>
-  count(bathrooms_text) |>
-  arrange(-n) |>
-  view()
-
-# Target variable distribution
-air_bnb_data |>
-ggplot(aes (x = host_is_superhost, fill = host_is_superhost) ) +
-  geom_bar () +
-  labs (y = "Count" , x = "Superhost Status") +
-  theme_classic() + theme (legend.position = "none") +
-  scale_fill_manual(values = c("darkgreen", "green"))
-
-# Check correlations-----------
-cor_matrix <- air_bnb_data |>
-  select (where(is.numeric) | where(is.logical)) |>
-  na.omit() |> 
-  cor() |>
-  as_tibble( )
-
-cor_matrix$varl <- 
-  colnames(cor_matrix)
-
 # training data tidy-----
 training_data <- air_bnb_data |>
   mutate(
@@ -63,10 +31,10 @@ training_data <- air_bnb_data |>
     bathrooms = if_else(str_detect(bathrooms_text, "alf"), 0.5, parse_number(bathrooms_text)),
 
     ### eliminating logicals -----
-    host_has_profile_pic = factor(host_has_profile_pic, levels = c(1, 0)),
-    host_identity_verified = factor(host_identity_verified, levels = c(1, 0)), 
-    has_availability = factor(has_availability, levels = c(1, 0)),
-    instant_bookable = factor (instant_bookable, levels = c(1, 0)),
+    #host_has_profile_pic = factor(host_has_profile_pic, levels = c(1, 0)),
+    #host_identity_verified = factor(host_identity_verified, levels = c(1, 0)), 
+    #has_availability = factor(has_availability, levels = c(1, 0)),
+    #instant_bookable = factor (instant_bookable, levels = c(1, 0)),
     
     ### managing dates -----
     host_since = year(host_since),
@@ -88,10 +56,10 @@ testing_data <- air_bnb_test_data |>
        host_response_rate = as.numeric(sub("%", "", host_response_rate)),
        host_acceptance_rate = as.numeric(sub("%","", host_acceptance_rate)),
        bathrooms = if_else(str_detect (bathrooms_text, "alf"), 0.5, parse_number (bathrooms_text)),
-       host_has_profile_pic = factor (host_has_profile_pic, levels = c(1, 0)),
-       host_identity_verified = factor(host_identity_verified, levels = c(1, 0)),
-       has_availability = factor(has_availability, levels = c(1, 0)),
-       instant_bookable = factor(instant_bookable, levels = c(1, 0)),
+       #host_has_profile_pic = factor (host_has_profile_pic, levels = c(1, 0)),
+       #host_identity_verified = factor(host_identity_verified, levels = c(1, 0)),
+       #has_availability = factor(has_availability, levels = c(1, 0)),
+       #instant_bookable = factor(instant_bookable, levels = c(1, 0)),
        host_since = year(host_since),
        year_since = factor(host_since - 0),
        last_review = year(last_review),
@@ -102,8 +70,8 @@ testing_data <- air_bnb_test_data |>
   select (-c(bathrooms_text, host_neighbourhood, host_since, last_review, first_review, host_location))
 
 # Save out Cleaned Data
-save(training_data, file = here("04_attempt/data/training_data.rda"))
-save(testing_data, file = here("04_attempt/data/testing_data.rda"))
+save(training_data, file = here("05_attempt/data/training_data.rda"))
+save(testing_data, file = here("05_attempt/data/testing_data.rda"))
        
 
 
--- 05_attempt/1_set_up.R
+++ 05_attempt/1_set_up.R
@@ -11,11 +11,11 @@ library(ggplot2)
 tidymodels_prefer()
 
 # read in training data -------
-load(here("04_attempt/data/training_data.rda"))
+load(here("05_attempt/data/training_data.rda"))
 
 # create resamples (10-fold cv with 4 repeats) ----------
-set.seed(1436)
+set.seed(1216)
 air_bnb_folds <- training_data |>
   vfold_cv(v = 10, repeats = 5, host_is_superhost)
 
-save(air_bnb_folds, file = here("04_attempt/data/air_bnb_folds.rda"))
+save(air_bnb_folds, file = here("05_attempt/data/air_bnb_folds.rda"))
