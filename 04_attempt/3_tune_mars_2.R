4a9321f8501e6dfde4fc1df4c50938fccc38f58c
Nikole26 <nikolemonterocervantes2026@u.northwestern.edu>
2024-05-15 08:59:44 -0500

    finish tuning and models with recipe 2 - attempt 4

---
+++ 04_attempt/3_tune_mars_2.R
@@ -0,0 +1,64 @@
+# L05 Feature Engineering II ----
+# Tuning for MARS FE model  ----
+
+# Load package(s) ----
+library(tidyverse)
+library(tidymodels)
+library(tictoc)
+library(here)
+library(parallel)
+library(doParallel)
+
+# Handle conflicts
+tidymodels_prefer()
+
+# parallel processing ----
+num.cores <- (detectCores(logical = TRUE)/2)
+registerDoParallel(cores = num.cores)
+
+# load resamples ----
+load(here("04_attempt/data/air_bnb_folds.rda"))
+
+# load preprocessing/recipe ----
+load(here("04_attempt/recipes/recipe_2.rda"))
+
+# model specifications ----
+mars_spec <- mars(
+  num_terms = tune(),
+  prod_degree = tune()
+) |>
+  set_mode("classification") |>
+  set_engine("earth")
+
+# define workflow ----
+mars_wflow <-
+  workflow() |>
+  add_model(mars_spec) |>
+  add_recipe(recipe_2)
+
+# hyperparameter tuning values ----
+mars_params <- hardhat::extract_parameter_set_dials(mars_spec) |>
+  update(
+    num_terms = num_terms(range = c(1L, 5L))
+  )
+
+# build grid
+mars_grid <- grid_regular(
+  mars_params,
+  levels = c(
+    "num_terms" = 5,
+    "prod_degree" = 2)
+)
+
+# tuning code in here
+tune_mars_2 <- tune_grid(
+  mars_wflow,
+  resamples = air_bnb_folds,
+  grid = mars_grid,
+  control = control_grid(save_workflow = TRUE)
+)
+# write out results (fitted/trained workflows & runtime info) ----
+save(
+  tune_mars_2,
+  file = here("04_attempt/results/tune_mars_2.rda")
+)
---
+++ 04_attempt/3_tune_nn_2.R
@@ -0,0 +1,56 @@
+# L04 Feature Engineering I ----
+# Single layer neural net tuning, simple imputation ----
+
+# Load package(s) ----
+library(tidyverse)
+library(tidymodels)
+library(tictoc)
+library(here)
+library(parallel)
+library(doParallel)
+
+# Handle conflicts
+tidymodels_prefer()
+
+# parallel processing ----
+num.cores <- detectCores((logical = TRUE)/2)
+registerDoParallel(cores = num.cores)
+
+# load resamples ----
+load(here("04_attempt/data/air_bnb_folds.rda"))
+
+# load preprocessing/recipe ----
+load(here("04_attempt/recipes/recipe_2.rda"))
+
+# model specifications ----
+nn_model <- mlp(
+  mode = "classification", 
+  hidden_units = tune(),
+  penalty = tune()
+) %>%
+  set_engine("nnet")
+
+# define workflow ----
+nn_wflow <-
+  workflow() |>
+  add_model(nn_model) |>
+  add_recipe(recipe_2)
+
+# hyperparameter tuning values ----
+nn_params <- extract_parameter_set_dials(nn_model)
+nn_grid <- grid_regular(nn_params, levels = 5)
+
+# tuning code in here
+set.seed(712)
+nn_tune_2 <- tune_grid(
+  nn_wflow,
+  resamples = air_bnb_folds,
+  grid = nn_grid,
+  control = control_grid(save_workflow = TRUE)
+)
+
+# write out results (fitted/trained workflows & runtime info) ----
+save(
+  nn_tune_2,
+  file = here("04_attempt/results/nn_tune_2.rda")
+)
