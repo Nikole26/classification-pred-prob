18191fd1d5b0e38808e24bbe66dad2b2163f1973
Nikole26 <nikolemonterocervantes2026@u.northwestern.edu>
2024-05-14 20:44:12 -0500

    recipe 2 - attempt 4

--- 04_attempt/2_recipes.R
+++ 04_attempt/2_recipes.R
@@ -26,9 +26,24 @@ recipe_1 <- recipe(host_is_superhost ~ ., data = training_data) |>
   step_nzv(all_numeric_predictors()) |>
   step_normalize(all_numeric_predictors())
 
-recipe_1 |>
+# Recipe 1
+recipe_2 <- recipe(host_is_superhost ~ ., data = training_data) |>
+  step_rm(id, host_verifications, host_response_time, beds, first_review_year, last_review_year, 
+          host_has_profile_pic, host_identity_verified, has_availability, instant_bookable, 
+          longitude, latitude, reviews_per_month, neighbourhood_cleansed, property_type, room_type,
+          availability_30, availability_60, availability_90, availability_365,
+          bathrooms, has_availability, minimum_nights, maximum_nights, minimum_maximum_nights, 	
+          maximum_maximum_nights) |>
+  step_impute_mean(all_numeric_predictors()) |>
+  step_impute_mode(all_nominal_predictors()) |>
+  step_dummy(all_nominal_predictors(), one_hot = TRUE) |>
+  step_nzv(all_numeric_predictors()) |>
+  step_normalize(all_numeric_predictors())
+
+recipe_2 |>
   prep() |>
   bake(new_data = NULL) |>
   glimpse()
 
 save(recipe_1, file = here("04_attempt/recipes/recipe_1.rda"))
+save(recipe_2, file = here("04_attempt/recipes/recipe_2.rda"))
