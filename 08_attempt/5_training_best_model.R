521b20c4ced4e357c85efb7c55d4ed03e428e177
Nikole26 <nikolemonterocervantes2026@u.northwestern.edu>
2024-05-19 20:17:23 -0500

    attempt 8 predictions

--- 08_attempt/5_training_best_model.R
+++ 08_attempt/5_training_best_model.R
@@ -24,3 +24,22 @@ final_fit_rf <-  fit(final_wflow_roc, training_data)
 
 # saving results-------
 save(final_fit_rf, file = here("08_attempt/results/final_fit_rf.rda"))
+
+# loading necessary data
+load(here("08_attempt/results/rf_tune_3.rda"))
+load(here("08_attempt/data/training_data.rda"))
+
+# Best Model --------
+select_best(rf_tune_3, metric = "roc_auc")
+
+# finalize workflow for roc-----
+final_wflow_roc <- rf_tune_3 |>
+  extract_workflow(rf_tune_3) |>
+  finalize_workflow(select_best(rf_tune_3, metric = "roc_auc"))
+
+# train final model----
+set.seed(120)
+final_fit_rf_1 <-  fit(final_wflow_roc, training_data)
+
+# saving results-------
+save(final_fit_rf_1, file = here("08_attempt/results/final_fit_rf_1.rda"))
--- 08_attempt/6_producing_submissions.R
+++ 08_attempt/6_producing_submissions.R
@@ -8,12 +8,12 @@ library(here)
 
 # loading necessary data
 load(here("08_attempt/data/testing_data.rda"))
-load(here("08_attempt/results/final_fit_rf.rda"))
+load(here("08_attempt/results/final_fit_rf_1.rda"))
 
 # producing predictions
-attempt_08_rf_1 <- testing_data |>
-  bind_cols(predict(final_fit_rf, testing_data, type = "prob")) |>
+attempt_08_rf_3 <- testing_data |>
+  bind_cols(predict(final_fit_rf_1, testing_data, type = "prob")) |>
   rename(predicted = .pred_1) |>
   select(id, predicted)
 
-write_csv(attempt_08_rf_1, here("submissions/attempt_08_rf_1.csv"))
+write_csv(attempt_08_rf_3, here("submissions/attempt_08_rf_3.csv"))
