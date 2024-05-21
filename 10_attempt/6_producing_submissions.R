0064477795a3a658af635b6c662ba4d7473a256b
Nikole26 <nikolemonterocervantes2026@u.northwestern.edu>
2024-05-21 18:15:53 -0500

    attempt 10 - submissions

--- 10_attempt/6_producing_submissions.R
+++ 10_attempt/6_producing_submissions.R
@@ -7,13 +7,13 @@ library(tidymodels)
 library(here)
 
 # loading necessary data
-load(here("09_attempt/data/testing_data.rda"))
-load(here("09_attempt/results/final_fit_rf.rda"))
+load(here("10_attempt/data/testing_data.rda"))
+load(here("10_attempt/results/final_fit_bt.rda"))
 
 # producing predictions
-attempt_09_rf_1 <- testing_data |>
-  bind_cols(predict(final_fit_rf, testing_data, type = "prob")) |>
+attempt_10_bt_2 <- testing_data |>
+  bind_cols(predict(final_fit_bt, testing_data, type = "prob")) |>
   rename(predicted = .pred_1) |>
   select(id, predicted)
 
-write_csv(attempt_09_rf_1, here("submissions/attempt_09_rf_1.csv"))
+write_csv(attempt_10_bt_2, here("submissions/attempt_10_bt_2.csv"))
