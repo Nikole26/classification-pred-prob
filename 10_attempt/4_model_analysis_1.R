528fcd919e749c43678cf571a90b39ce75ce88e0
Nikole26 <nikolemonterocervantes2026@u.northwestern.edu>
2024-05-24 18:58:43 -0500

    10 attempt saving results

--- 10_attempt/4_model_analysis_1.R
+++ 10_attempt/4_model_analysis_1.R
@@ -12,15 +12,19 @@ tidymodels_prefer()
 # load data
 load(here("10_attempt/results/rf_tune_1.rda"))
 load(here("10_attempt/results/bt_tune_1.rda"))
+load(here("10_attempt/results/rf_tune_2.rda"))
+load(here("10_attempt/results/bt_tune_2.rda"))
 
 # comparison table
 model_set <-
   as_workflow_set(
     "bt_1" = bt_tune_1,
     "rf_1" = rf_tune_1,
+    "bt_2" = bt_tune_2,
+    "rf_2" = rf_tune_2,
   )
 
-models_table_1 <- model_set |> 
+models_table <- model_set |> 
   collect_metrics() |>
   filter(.metric == "roc_auc") |>
   arrange(desc(mean)) |>
@@ -28,8 +32,12 @@ models_table_1 <- model_set |>
   slice_max(mean, with_ties = FALSE) |>
   select(wflow_id, roc_auc = mean, std_err, n, model) |>
   ungroup() |>
-  mutate(recipe = c("recipe 1")) |> 
+  mutate(recipe = c("recipe 1", "recipe 1", "recipe 2", "recipe 2")) |> 
   select(-wflow_id) |>
   arrange(desc(roc_auc))
 
-save(models_table_1, file = here("09_attempt/results/models_table_1.rda"))
+bt_autoplot <- autoplot(bt_tune_2, metric = "roc_auc") +
+  theme_minimal()
+
+save(models_table, file = here("10_attempt/results/models_table.rda"))
+save(bt_autoplot, file = here("10_attempt/results/bt_autoplot.rda"))
